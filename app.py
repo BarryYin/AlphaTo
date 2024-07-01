243d9638d8c723566410516b8595ab677a3e196d
BarryYin <1509008060@qq.com>
2024-07-01 10:27:16 +0800

    fix
    1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 #os.system('pip install -e ./agentscope')
 base_path = './internlm2-chat-7b'
+os.system(f'git lfs install --local')
 os.system(f'git clone https://code.openxlab.org.cn/OpenLMLab/internlm2-chat-7b.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 os.system(f'lmdeploy serve api_server {base_path}  --server-port 23333')
